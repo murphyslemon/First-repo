e81dc8015afcff9457817c22eb37dd088d1aeb50
First code <Francesco.Natanni@metropolia.fi>
2022-08-22 20:12:23 +0300

    First day in school

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-#print("hello everyone")
+print("hello everyone")
 
-#print("good")
-#print("morning")
+print("good")
+print("morning")
 
-#first_number = 3
-#print (first_number)
+first_number = 3
+print(first_number)
 
 fahrenheit_str = input("Temperature in fahrenheit:")
 fahrenheit = float(fahrenheit_str)
 celsius = (fahrenheit - 32)*5/9
 print(f"Temperature in celsius:{celsius:6.2f}")
 
-#height_str = input("Height in metres:")
-#weight_str = input("Weight in kilograms:")
-#height = float(height_str)
-#weight = float(weight_str)
-#BMI = weight/(height*height)
-#print(f"This is your BMI:{BMI:6.1f}")
+height_str = input("Your height in metres:")
+weight_str = input("Your weight in kilograms:")
+height = float(height_str)
+weight = float(weight_str)
+BMI = weight/(height*height)
+print(f"This is your BMI:{BMI:6.1f}")
