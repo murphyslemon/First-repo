abac82f598d06726c058af4308c41e497a2053bc
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-10-25 23:38:25 +0300

    organising

--- period1/test 2.py
+++
@@ -1,9 +0,0 @@
-seasons_of_the_year = ("Spring", "Summer", "Autumn", "Winter")
-month = ([3, 4, 5], [6, 7, 8], [9, 10, 11], [12, 1, 2])
-
-print(len(month))
-month_number = int(input("Enter the month number(1-12): "))
-
-for i in range(len(month)):
-    if month_number in month[i]:
-        print(seasons_of_the_year[i])
