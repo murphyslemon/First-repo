45aedfb191c0de241f433f3124118c7110b12c06
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-11-06 15:47:10 +0200

    ex10Q4

--- Period2/Exercises/exercise#10.py
+++ Period2/Exercises/exercise#10.py
@@ -1,30 +1,30 @@
 #problem 1
-class Elevator:
-    def __init__(self):
-        self.floor = 0
-
-    def go_to_floor(self, level):
-        print(self.floor)
-        while 1:
-            if self.floor > level:
-                self.floor_down()
-                print(self.floor)
-            elif self.floor < level:
-                self.floor_up()
-                print(self.floor)
-            else:
-                break
-
-    def floor_up(self):
-        self.floor += 1
-
-    def floor_down(self):
-        self.floor -= 1
-
-e = Elevator()
-
-e.go_to_floor(-3)
-e.go_to_floor(0)
+# class Elevator:
+#     def __init__(self):
+#         self.floor = 0
+#
+#     def go_to_floor(self, level):
+#         print(self.floor)
+#         while 1:
+#             if self.floor > level:
+#                 self.floor_down()
+#                 print(self.floor)
+#             elif self.floor < level:
+#                 self.floor_up()
+#                 print(self.floor)
+#             else:
+#                 break
+#
+#     def floor_up(self):
+#         self.floor += 1
+#
+#     def floor_down(self):
+#         self.floor -= 1
+#
+# e = Elevator()
+#
+# e.go_to_floor(-3)
+# e.go_to_floor(0)
 
 #problem 2
 # class Building:
@@ -45,40 +45,98 @@ e.go_to_floor(0)
 # print(b1.elevator_list[1].floor)
 
 #problem 3
-class Building:
-    def __init__(self, bottom_floor, top_floor, number_of_elevators):
-        self.elevator_list = []
-        self.bottom_floor = bottom_floor
-        self.top_floor = top_floor
-        self.number_of_elevators = number_of_elevators
-        for i in range(number_of_elevators):
-            self.elevator_list.append(Elevator())
-
-    def run_elevator(self, elevator_number, floor):
-        self.elevator_list[elevator_number].go_to_floor(floor)
-
-    def fire_alarm(self):
-        for y in self.elevator_list:
-            y.go_to_floor(self.bottom_floor)
-
-b1 = Building(-3, 10, 2)
-
-b1.run_elevator(1, 4)
-print(b1.elevator_list[1].floor)
-
-b1.fire_alarm()
-for x in range(len(b1.elevator_list)):
-    print(f"Elevator {x+1} is on floor {b1.elevator_list[x].floor}.")
+# class Building:
+#     def __init__(self, bottom_floor, top_floor, number_of_elevators):
+#         self.elevator_list = []
+#         self.bottom_floor = bottom_floor
+#         self.top_floor = top_floor
+#         self.number_of_elevators = number_of_elevators
+#         for i in range(number_of_elevators):
+#             self.elevator_list.append(Elevator())
+#
+#     def run_elevator(self, elevator_number, floor):
+#         self.elevator_list[elevator_number].go_to_floor(floor)
+#
+#     def fire_alarm(self):
+#         for y in self.elevator_list:
+#             y.go_to_floor(self.bottom_floor)
+#
+# b1 = Building(-3, 10, 2)
+#
+# b1.run_elevator(1, 4)
+# print(b1.elevator_list[1].floor)
+#
+# b1.fire_alarm()
+# for x in range(len(b1.elevator_list)):
+#     print(f"Elevator {x+1} is on floor {b1.elevator_list[x].floor}.")
 
 #problem 4
-from exercise9 import Car
+import random
+from prettytable import PrettyTable
+
+class Car:
+    def __init__(self, registration_number, maximum_speed: int):
+        self.registration_number = registration_number
+        self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.distance = 0
+
+    def accelerate(self, change_of_speed):
+        speed = self.current_speed + change_of_speed
+        if speed < self.maximum_speed and speed > 0:
+            self.current_speed += change_of_speed
+        elif speed < 0:
+            self.current_speed = 0
+        if speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+
+    def __str__(self):
+        return "Current speed is: " + str(self.current_speed)
+
+    def drive(self, number_of_hours):
+        self.distance += self.current_speed * number_of_hours
+
 class Race:
-    def __init__(self, name, race_distance, car_list):
-        pass
+    def __init__(self, race_name, race_distance, car_list):
+        self.race_name = race_name
+        self.race_distance = race_distance
+        self.car_list = car_list
 
     def hour_passes(self):
-        Car.accelerate(self.car_list)
-        for z in self.car_list:
-            z.drive(1)
+        for car in self.car_list:
+            car.accelerate(random.randint(-10, 15))
+            car.drive(1)
     
-    def race_finished():
+    def race_finished(self):
+        for car in self.car_list:
+            if car.distance > self.race_distance:
+                return True
+        return False
+
+    def print_status(self):
+        x = PrettyTable()
+        x.field_names = ["Registration Number", "Distance", "Current Speed"]
+        for car in list_of_cars:
+            x.add_row([car.registration_number, car.distance, car.current_speed])
+        print(x.get_string(sortby="Distance", reversesort=True))
+
+list_of_cars = []
+
+for i in range(10):
+    list_of_cars.append(Car("ABC-" + str(i+1), random.randint(100, 200)))
+
+r1 = Race("Grand Demolition Derby",8000, list_of_cars)
+print(f"{r1.race_name} is {r1.race_distance}kms long.")
+
+print("Starting race")
+
+hour = 0
+while not r1.race_finished():
+    r1.hour_passes()
+    hour += 1
+
+    if hour % 10 == 0:
+        print(f"After {hour}hrs")
+        r1.print_status()
+print(f"final result after {hour}hrs is: ")
+r1.print_status()
--- Period2/Exercises/testing2.py
+++ Period2/Exercises/testing2.py
@@ -1,43 +1,116 @@
-class Elevator:
-  def __init__(self):
-      self.floor = 0
+import random
 
-  def floor_up(self):
-      self.floor += 1
 
-  def floor_down(self):
-      self.floor -= 1
+class Car:
+    def __init__(self, registration, max_speed):
+        self.registration = registration
+        self.max_speed = max_speed
+        self.current_speed = 0
+        self.distance = 0
 
-  def go_to_floor(self,f):
-      while 1:
-          if self.floor > f:
-              self.floor_down()
-          elif self.floor < f:
-              self.floor_up()
-          else:
-              break
+    def accelerate(self, add_speed):
+        speed = self.current_speed + add_speed
+        if self.max_speed > speed and speed > 0:
+            self.current_speed += add_speed
+        elif speed < 0:
+            self.current_speed = 0
+        else:
+            pass
+        # print("Too fast")
 
-e = Elevator()
+    def run(self, h):
+        self.distance += self.current_speed * h
 
-e.go_to_floor(5)
-print(e.floor)
-e.go_to_floor(0)
-print(e.floor)
 
-#2,3
-class Building:
-    def __init__(self, n):
-        self.elevators = []
-        for i in range(n):
-            self.elevators.append(Elevator())
-    def run_elevator(self, el_num, f):
-        self.elevators[el_num].go_to_floor(f)
+c1 = Car("ABC-123", 142)
 
-    def fire_alarm(self):
-        for _i in self.elevators:
-            _i.go_to_floor(0)
-        
-b = Building(100)
+print(c1.registration)
+print(c1.max_speed)
+print(c1.current_speed)
+print(c1.distance)
 
-b.run_elevator(50, 120)
-print(b.elevators[50].floor)
+c1.accelerate(+30)
+c1.accelerate(+70)
+c1.accelerate(+50)
+c1.accelerate(-200)
+
+print(c1.current_speed)
+
+c1.accelerate(60)
+c1.distance = 2000
+c1.run(1.5)
+
+print(c1.distance)
+
+
+# 4
+
+def update_speed(car_list):
+    for _i in car_list:
+        _i.accelerate(random.randint(-10, 15))
+        _i.run(1)
+
+
+def print_properties(car_list):
+    for _i in car_list:
+        print(_i.distance, _i.current_speed)
+
+
+def check_if_goal(car_list):
+    for _i in car_list:
+        if _i.distance > 10000:
+            return True
+    return False
+
+
+car_list = []
+for i in range(10):
+    car_list.append(Car("ABC-" + str(i + 1), random.randint(0, 100)))
+h = 0
+while 1:
+    if check_if_goal(car_list):
+        break
+    update_speed(car_list)
+
+    # print(h)
+    h += 1
+
+print_properties(car_list)
+
+
+# EXERSICE 10 Section 4
+
+class Race:
+    def __init__(self, race_name, race_km, car_list):
+        self.race_name = race_name
+        self.race_km = race_km
+        self.car_list = car_list
+
+    def hour_passes(self):
+        update_speed(self.car_list)
+        for _i in self.car_list:
+            _i.run(1)
+
+    def race_finished(self):
+        for _i in self.car_list:
+            if _i.distance < self.race_km:
+                return False
+        return True
+
+    def __repr__(self):
+        text = "speed (km/h) |  distance (km)\n"
+        for _i in self.car_list:
+            text += str(_i.current_speed) + "km/h | " + str(_i.distance) + "km\n"
+        return text
+
+
+r = Race("Grand Demolition Derby", 10000, car_list)
+print("Starting race")
+for i in range(100000):
+    print(i)
+    if r.race_finished():
+        break
+    r.hour_passes()
+
+    if i % 10 == 0:
+        print(r)
