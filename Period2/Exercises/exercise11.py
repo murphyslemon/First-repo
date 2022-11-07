6decb37ac3a19f58cec1740fc8a469cd5ea8ce7e
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-11-07 11:27:45 +0200

    ex11

--- Period2/Exercises/exercise11.py
+++ Period2/Exercises/exercise11.py
@@ -25,3 +25,31 @@ m1.print_information()
 
 b1 = Book("Compartment No. 6", "Rosa Liksom", "192")
 b1.print_information()
+
+#problem 2
+from exercise10 import Car
+class ElectricCar(Car):
+    def __init__(self, registration_number, maximum_speed, battery_capacity):
+        self.battery_capacity = battery_capacity
+        super().__init__(registration_number, maximum_speed)
+
+class GasolineCar(Car):
+    def __init__(self, registration_number, maximum_speed, fuel_tank_size):
+        self.fuel_tank_size = fuel_tank_size
+        super().__init__(registration_number, maximum_speed,)
+
+E1 = ElectricCar("ABC-15", 180, 52.5)
+G1 = GasolineCar("ACD-123", 162, 32.3)
+
+E1.accelerate(150)
+E1.drive(3)
+print(f"Electric Car: {E1.registration_number} "
+      f"\nCurrent speed: {E1.current_speed}km/h "
+      f"\nDistance: {E1.distance}km")
+
+G1.accelerate(120)
+G1.drive(3)
+print(f"Gasoline Car: {G1.registration_number} "
+      f"\nCurrent speed: {G1.current_speed}km/h "
+      f"\nDistance: {G1.distance}km")
+
