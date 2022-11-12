e8dcd6d13143f06a82aaaa13c02cb58f6c71d712
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-11-12 22:53:16 +0200

    ex12 p2

--- Period2/Exercises/exercise12.py
+++ Period2/Exercises/exercise12.py
@@ -1,18 +1,21 @@
-import requests, json
+import requests
+import json
+from api_storage import ex12_api_key
 
-#Problem 1
-request = "https://api.chucknorris.io/jokes/random"
-response = requests.get(request).json()
-print(response["value"])
+# Problem 1
+request1 = "https://api.chucknorris.io/jokes/random"
+try:
+    response = requests.get(request1).json()
+    print(f"Your random and terrible Chuck Norris joke of the day is: \n{response['value']}")
+except requests.exceptions.RequestException as e:
+    print("Request could not be completed.")
 
-#Problem 2
-location_name = input("Please enter city name, state code or country code: ")
-request2 = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}"
-#weather from lat and long
-https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+# Problem 2
+location_name = input("Please enter city name: ")
 
-#lat and long convert to city name
-http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+request2 = "https://api.openweathermap.org/data/2.5/weather?q=" + location_name + "&appid=" + ex12_api_key + "&units=metric"
+response2 = requests.get(request2).json()
 
-api key?
-873df6e3e029ba2d8538f1482461817f
+for a in response2["weather"]:
+    print(f"The weather in {location_name} is {a['description']} and the temperature is {response2['main']['temp']} degrees")
+
