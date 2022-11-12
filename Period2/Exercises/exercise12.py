74c60f52e99853a586bf89ddc3ed5ff79133b58d
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-11-12 23:43:12 +0200

    ex12 p2

--- Period2/Exercises/exercise12.py
+++ Period2/Exercises/exercise12.py
@@ -1,21 +1,31 @@
 import requests
 import json
+import textwrap
 from api_storage import ex12_api_key
 
 # Problem 1
 request1 = "https://api.chucknorris.io/jokes/random"
 try:
-    response = requests.get(request1).json()
-    print(f"Your random and terrible Chuck Norris joke of the day is: \n{response['value']}")
+    response1 = requests.get(request1)
+    if response1.status_code == 200:
+        json_response1 = response1.json()
+        print("Your random and terrible Chuck Norris joke of the day is: ")
+        print(textwrap.fill(json_response1['value'], width=100))
+
 except requests.exceptions.RequestException as e:
     print("Request could not be completed.")
 
 # Problem 2
 location_name = input("Please enter city name: ")
-
 request2 = "https://api.openweathermap.org/data/2.5/weather?q=" + location_name + "&appid=" + ex12_api_key + "&units=metric"
-response2 = requests.get(request2).json()
 
-for a in response2["weather"]:
-    print(f"The weather in {location_name} is {a['description']} and the temperature is {response2['main']['temp']} degrees")
+try:
+    response2 = requests.get(request2)
+    if response2.status_code == 200:
+        json_response2 = response2.json()
+        for a in json_response2["weather"]:
+            print(
+                f"The weather in {location_name} is {a['description']} and the temperature is {json_response2['main']['temp']} degrees")
 
+except requests.exceptions.RequestException as e:
+    print("Request could not be completed.")
