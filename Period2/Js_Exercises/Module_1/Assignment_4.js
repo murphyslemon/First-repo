3e784fa478eccef859571a2b899b9fcdff481589
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-11-15 22:39:05 +0200

    assignment4

--- Period2/Js_Exercises/Module_1/Assignment_4.js
+++ Period2/Js_Exercises/Module_1/Assignment_4.js
@@ -1,1 +1,20 @@
 'use strict';
+let studentName = prompt("What is your name?");
+let house = Math.floor(Math.random()*4);
+console.log(house)
+
+if (house === 0) {
+    document.querySelector('#printing').innerHTML = `${studentName}, you are in Griffindor.`;
+}
+
+if (house === 1) {
+    document.querySelector('#printing').innerHTML = `${studentName}, you are in Slytherin.`;
+}
+
+if (house === 2) {
+    document.querySelector('#printing').innerHTML = `${studentName}, you are in Huffelpuff.`;
+}
+
+if (house === 3) {
+    document.querySelector('#printing').innerHTML = `${studentName}, you are in Ravenclaw.`;
+}
