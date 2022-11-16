f8c1b33095ce1d9c886c6b2aadcbb19e4c657da7
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-11-16 22:20:25 +0200

    assignment6

--- Period2/Js_Exercises/Module_1/Assignment_6.js
+++ Period2/Js_Exercises/Module_1/Assignment_6.js
@@ -1,1 +1,14 @@
 'use strict';
+let answer = confirm("Should I calculate the square root?");
+if (answer === true) {
+    let number = prompt("What number do you want to square root?");
+    if (number < 0) {
+        document.querySelector('#printing').innerHTML = `The square root of a negative number is not defined.`;
+    } else {
+        let squareRoot = Math.sqrt(number);
+        document.querySelector('#printing').innerHTML = `The square root of ${number} is ${squareRoot}.`;
+    }
+}
+if (answer === false) {
+    document.querySelector('#printing').innerHTML = `The square root is not calculated.`;
+}
