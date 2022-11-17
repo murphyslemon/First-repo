f8b7c401f1a1335e9d9aef967af1af38ee686cc7
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-11-17 13:25:01 +0200

    assignment 8

--- Period2/Js_Exercises/Module_1/Assignment_8.js
+++ Period2/Js_Exercises/Module_1/Assignment_8.js
@@ -1,14 +1,12 @@
 'use strict';
-let unorderedYearList = []
 let startYear = +prompt("What is your starting year?");
 let endYear = +prompt("What is your ending year?");
 const ul = document.createElement("ul");
+
 for (let i = startYear; i <= endYear; i++){
     if ((i % 4 === 0) && (i % 100 !== 0) || (i % 400 === 0)) {
-      ul.innerHTML += `<li> ${i} </li>`
+        ul.innerHTML += `<li> ${i} </li>`
     }
 }
-
-console.log(unorderedYearList);
 
 document.body.appendChild(ul)
