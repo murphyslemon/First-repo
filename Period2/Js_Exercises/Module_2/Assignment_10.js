a40e99e6010465ac3c781481391cf3ea5533e867
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-11-23 21:44:15 +0200

    assignment10

--- Period2/Js_Exercises/Module_2/Assignment_10.js
+++ Period2/Js_Exercises/Module_2/Assignment_10.js
@@ -1,1 +1,18 @@
 'use strict';
+let totalCandidates = +prompt("How many candidates are there?");
+let listOfCandidates = []
+
+class Candidate {
+    constructor(votes = 0) {
+        this.name = prompt("Enter a candidate's name:");
+        this.votes = votes;
+    }
+}
+
+var candidate = []
+for (let i = 0; i < totalCandidates; i++) {
+    candidate[i+1] = new Candidate();
+    listOfCandidates.push(candidate[i+1])
+    console.log(candidate[i+1])
+}
+//unfinished
