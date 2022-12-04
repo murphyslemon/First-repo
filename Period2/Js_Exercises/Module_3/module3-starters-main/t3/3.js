70206d7e7a587a897606359ed26add74db40bd34
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-12-04 20:22:38 +0200

    assignment4

--- Period2/Js_Exercises/Module_3/module3-starters-main/t3/3.js
+++ Period2/Js_Exercises/Module_3/module3-starters-main/t3/3.js
@@ -2,9 +2,16 @@
 const names = ['John', 'Paul', 'Jones'];
 const ul = document.querySelector('#target');
 
-names.forEach(function (item,index) {
+//testing another way
+// names.forEach(function (item,index) {
+//     const li = document.createElement('li');
+//     li.innerHTML=names[index];
+//     ul.appendChild(li);
+// })
+
+for (let i=0; i<names.length; i++) {
     const li = document.createElement('li');
-    li.innerHTML=names[index];
+    li.innerHTML=names[i];
     ul.appendChild(li);
-})
+}
 
