9fa52b3931b40ee4246ed5a46ca0c017720c42fa
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-12-05 11:43:25 +0200

    assignment 8

---
+++ Period2/Js_Exercises/Module_3/module3-starters-main/t8/8.js
@@ -0,0 +1,30 @@
+const button = document.querySelector('#start');
+const p = document.querySelector('#result');
+const select = document.querySelector("#operation");
+
+
+
+button.addEventListener('click', operations);
+
+function operations() {
+    let num1 = +(document.querySelector('#num1').value);
+    let num2 = +(document.querySelector('#num2').value);
+ 
+
+    if (select.value === 'add'){
+        let result = num1 + num2;
+        p.innerHTML = result;
+    }
+    if (select.value ==='sub'){
+        let result = num1 - num2;
+        p.innerHTML = result;
+    }
+    if (select.value === 'multi'){
+        let result = num1 * num2;
+        p.innerHTML = result;
+    }
+    if (select.value === 'div'){
+        let result = num1 / num2;
+        p.innerHTML = result;
+    } 
+}
