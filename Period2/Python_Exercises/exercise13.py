8b5bc5f96d4b46e748b90ba8b6af945b8eac877d
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-12-09 19:37:01 +0200

    js

--- Period2/Python_Exercises/exercise13.py
+++ Period2/Python_Exercises/exercise13.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, Response
 import json
 import mysql.connector
+from flask_cors import CORS
 
 app = Flask(__name__)
+corse = CORS(app)
 app.config["JSON_SORT_KEYS"] = False
 #
 # Problem 1
