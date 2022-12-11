d65a5c5dd2558bb66b74ff4b886e126ce099fbe8
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-12-12 00:57:07 +0200

    start step 4

---
+++ Period2/Js_Exercises/Module_4/step_4/module_4.js
@@ -0,0 +1,51 @@
+'use strict';
+// step 3
+document.addEventListener('submit', async function(evt) {
+    evt.preventDefault();
+    const inputValue = document.querySelector('#query').value;
+    try {
+        const proxy = 'https://api.allorigins.win/get?url=';
+        const search = 'https://api.tvmaze.com/search/shows?q='+ inputValue;
+        const url = proxy + encodeURIComponent(search);
+
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Invalid input!');
+        const result = await response.json();
+        const dataFromAPI = JSON.parse(result.contents);
+
+        for (let item of dataFromAPI) {
+            const container = document.createElement('show');
+            const h2 = document.createElement('h2');
+            h2.innerHTML = item['show']['name'];
+            container.appendChild(h2);
+
+            const image = document.createElement('img');
+            image.src = item['show']['image'] ?
+                item['show']['image']['medium'] :
+                'https://via.placeholder.com/243x342?text=image'
+            image.alt = item['show']['name'];
+            container.appendChild(image);
+
+            const genres = document.createElement('ul');
+            for (let i = 0; i <= item['show']['genres'].length; i++){
+                genres.innerHTML += `<li> ${item['show']['genres'][i]} </li>`
+                }
+            container.appendChild(genres)
+
+            const description = document.createElement('p');
+            description.innerHTML = item['show']['summary'];
+            container.appendChild(description);
+
+            const link = document.createElement('a');
+            link.href = item['show']['url'];
+            link.target = "_blank";
+            link.innerHTML = item['show']['url'];
+            container.appendChild(link);
+            document.body.appendChild(container)
+        }
+
+    } catch (error) {
+        console.log('error', error);
+    }
+});
+
