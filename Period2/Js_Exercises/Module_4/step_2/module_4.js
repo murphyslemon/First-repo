e47fea3678f14e29b107e94b950d9e6df219e166
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-12-11 15:00:54 +0200

    step2

--- Period2/Js_Exercises/Module_4/step_2/module_4.js
+++ Period2/Js_Exercises/Module_4/step_2/module_4.js
@@ -2,18 +2,32 @@
 // step 2
 document.addEventListener('submit', async function(evt) {
     evt.preventDefault();
-    const inputValue = document.querySelector('#query').value
+    const inputValue = document.querySelector('#query').value;
     try {
         const search = 'https://api.tvmaze.com/search/shows?q='+ inputValue;
         const response = await fetch(search);
         const json = await response.json();
-        console.log(json);
-        for (const item of json) {
-            console.log(item['show']['name'])
-            console.log(item['show']['url'])
-            console.log(item['show']['image']['medium'])
-            console.log(item['show']['summary'])
-        }
+
+        const container = document.getElementById('show');
+        const h2 = document.createElement('h2');
+        h2.innerHTML = json[0]['show']['name'];
+        container.appendChild(h2);
+
+        const image = document.createElement('img');
+        image.src = json[0]['show']['image']['medium'];
+        image.alt = json[0]['show']['name'];
+        container.appendChild(image);
+
+        const description = document.createElement('p');
+        description.innerHTML = json[0]['show']['summary'];
+        container.appendChild(description);
+
+        const url = document.createElement('a');
+        url.href = json[0]['show']['url'];
+        url.target = "_blank";
+        url.innerHTML = json[0]['show']['url'];
+        container.appendChild(url);
+
     } catch (e) {
         console.log('error', e);
     }
