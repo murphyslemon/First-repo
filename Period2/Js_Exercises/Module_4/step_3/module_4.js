1c08c61f4dc0ff2c0e2bb190f57df62c330cbff7
murphyslemon <111736225+murphyslemon@users.noreply.github.com>
2022-12-12 00:46:26 +0200

    step3

--- Period2/Js_Exercises/Module_4/step_3/module_4.js
+++ Period2/Js_Exercises/Module_4/step_3/module_4.js
@@ -1,35 +1,57 @@
 'use strict';
-// step 2
+// step 3
 document.addEventListener('submit', async function(evt) {
     evt.preventDefault();
     const inputValue = document.querySelector('#query').value;
     try {
+        const proxy = 'https://api.allorigins.win/get?url=';
         const search = 'https://api.tvmaze.com/search/shows?q='+ inputValue;
-        const response = await fetch(search);
-        const json = await response.json();
+        const url = proxy + encodeURIComponent(search);
 
-        const container = document.getElementById('show');
-        const h2 = document.createElement('h2');
-        h2.innerHTML = json[0]['show']['name'];
-        container.appendChild(h2);
+        const response = await fetch(url);
+        if (!response.ok) throw new Error('Invalid input!');
+        const result = await response.json();
+        const dataFromAPI = JSON.parse(result.contents);
 
-        const image = document.createElement('img');
-        image.src = json[0]['show']['image']['medium'];
-        image.alt = json[0]['show']['name'];
-        container.appendChild(image);
+        for (let item of dataFromAPI) {
+            const container = document.createElement('show');
+            const h2 = document.createElement('h2');
+            h2.innerHTML = item['show']['name'];
+            container.appendChild(h2);
 
-        const description = document.createElement('p');
-        description.innerHTML = json[0]['show']['summary'];
-        container.appendChild(description);
+            const image = document.createElement('img');
+            try {
+                image.src = item['show']['image']['medium'];
+                image.alt = item['show']['name'];
+                container.appendChild(image);
+                console.log('not null');
+            } catch {
+                console.log('null');
+                image.src = 'https://via.placeholder.com/243x342?text=image';
+                image.alt = item['show']['name'];
+                container.appendChild(image);
+            }
 
-        const url = document.createElement('a');
-        url.href = json[0]['show']['url'];
-        url.target = "_blank";
-        url.innerHTML = json[0]['show']['url'];
-        container.appendChild(url);
+            const genres = document.createElement('ul');
+            for (let i = 0; i <= item['show']['genres'].length; i++){
+                genres.innerHTML += `<li> ${item['show']['genres'][i]} </li>`
+                }
+            container.appendChild(genres)
 
-    } catch (e) {
-        console.log('error', e);
+            const description = document.createElement('p');
+            description.innerHTML = item['show']['summary'];
+            container.appendChild(description);
+
+            const link = document.createElement('a');
+            link.href = item['show']['url'];
+            link.target = "_blank";
+            link.innerHTML = item['show']['url'];
+            container.appendChild(link);
+            document.body.appendChild(container)
+        }
+
+    } catch (error) {
+        console.log('error', error);
     }
 });
 
--- Period2/Js_Exercises/testing.js
+++ Period2/Js_Exercises/testing.js
@@ -4,9 +4,7 @@ document.addEventListener('submit', async function(evt) {
     evt.preventDefault();
     const inputValue = document.querySelector('#input').value;
     try {
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
         const search = 'https://api.tvmaze.com/search/shows?q=' + inputValue;
-        const url = proxy + (search);
         const response = await fetch(search);
         if (!response.ok) throw new Error('Invalid input!');
         const json = await response.json();
@@ -14,7 +12,7 @@ document.addEventListener('submit', async function(evt) {
 
         const s = document.getElementById('shows');
         for (const pic of json) {
-            const show = pic["show"]
+            const show = pic['show'];
             const article = document.createElement('article');
             article.classList = ['card'];
             const h2 = document.createElement('h2');
@@ -22,24 +20,32 @@ document.addEventListener('submit', async function(evt) {
             article.appendChild(h2);
             const figure = document.createElement('figure');
             const img = document.createElement('img');
-            img.src = show['image']['medium'];
+            img.src = show['image'] ?
+                show['image']['medium'] :
+                'https://via.placeholder.com/243x342?text=' + show['name'];
             img.alt = show['name'];
             figure.appendChild(img);
             const figcaption = document.createElement('figcaption');
             const a = document.createElement('a');
-            a.href = show["url"]
-            a.target = "_blank"
-            a.innerHTML = "Learn more"
+            a.href = show['url'];
+            a.target = '_blank';
+            a.innerHTML = 'Learn more';
             figcaption.appendChild(a);
             figure.appendChild(figcaption);
             const p = document.createElement('p');
+            const p2 = document.createElement('p');
+            let genres = '| ';
+            for (const genre of show['genres']) {
+                genres += genre + ' | ';
+            }
+            p2.innerHTML = (genres !== '| ') ? genres : '| (no genre given) |';
             p.innerHTML = show['summary'];
             article.appendChild(figure);
+            article.appendChild(p2);
             article.appendChild(p);
             s.appendChild(article);
-            break;
         }
     } catch (e) {
-        console.log(error.message);
+        console.log('error', e);
     }
 });
